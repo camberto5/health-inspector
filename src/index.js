579f77a0ec6eddd3a11d1c067500794e06348703
Cameron Maberto <cmaberto@uci.edu>
2017-10-02 20:59:02 -0700

    hello world component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+
+    return (
+        <div>
+        <h1>Hello there World!</h1>
+        <p>Ever since I was a young boy I said "Hello World"</p>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
